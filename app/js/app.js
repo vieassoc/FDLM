0d3ab7d156b9577fdb89e8f32509853a6276c62e
serut <l.mieulet@gmail.com>
2014-03-07 11:14:54 +0100

    Add google analytics

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,18 @@
 'use strict';
+/* Google Analytics*/
+(function(angular) { 
+
+  angular.module('analytics', ['ng']).service('analytics', [
+    '$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+      var track = function() {
+        $window._gaq.push(['_trackPageview', $location.path()]);
+      };
+      $rootScope.$on('$viewContentLoaded', track);
+    }
+  ]);
+
+}(window.angular));
+/* end Google Analytics*/
 
 /* App Module */
 
@@ -8,6 +22,7 @@ var App = angular.module('fdlmApp', [
 	'appServices',
 	'leaflet-directive',
 	'angularSmoothscroll',
+	'analytics',
 ]);
 
 App.config(['$routeProvider',
