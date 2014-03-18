5f8fe4244b489d59dd904117b50e6e2bf2b82d47
serut <l.mieulet@gmail.com>
2014-03-18 08:59:04 +0100

    Add markers and fix template on association

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,99 +3,126 @@
 /* Controllers */
 
 var appControllers = angular.module('appControllers', [
-  'ngSanitize',
-  'leaflet-directive',
+	'ngSanitize',
+	'leaflet-directive',
 ]);
 
 appControllers.controller('MenuCtrl', ['$scope', 'Menu','$route',
-  function($scope, Menu, $route) {
-    $scope.menu = Menu.query();
-    $scope.isActive = function(part){
-      var current = window.location.href
-      var current_splited = current.split("/");
-      return ((current_splited[current_splited.length-2] === part) && (current_splited[current_splited.length-1] === "") || (current.indexOf(part.replace("#","")) > 0));
-    };
-  }]);
+	function($scope, Menu, $route) {
+	$scope.menu = Menu.query();
+	$scope.isActive = function(part){
+		var current = window.location.href
+		var current_splited = current.split("/");
+		return ((current_splited[current_splited.length-2] === part) && (current_splited[current_splited.length-1] === "") || (current.indexOf(part.replace("#","")) > 0));
+	};
+	}]);
 
 appControllers.controller('TemplateCtrl', ['$scope', 'GeneralInformation',
-  function($scope, GeneralInformation) {
-    $scope.general_info = GeneralInformation.query();
-  }]);
+	function($scope, GeneralInformation) {
+	$scope.general_info = GeneralInformation.query();
+	}]);
 
 appControllers.controller('NewsListCtrl', ['$scope', 'News', 'Menu', 'Gallery',
-  function($scope, News, Menu,Gallery) {
-    $scope.news = News.query();
-    $scope.menu = Menu.query();
-    $scope.gallery = Gallery.query({'idGallery':1});
-    $scope.getSrcYoutube = function(url){
-        return 'https://www.youtube-nocookie.com/embed/'+url;
-    }
-  }]);
+	function($scope, News, Menu,Gallery) {
+	$scope.news = News.query();
+	$scope.menu = Menu.query();
+	$scope.gallery = Gallery.query({'idGallery':1});
+	$scope.getSrcYoutube = function(url){
+		return 'https://www.youtube-nocookie.com/embed/'+url;
+	}
+	}]);
 
 appControllers.controller('AssociationCtrl', ['$scope', 'Staff',
-  function($scope, Staff) {
-    $scope.staff = Staff.query();
-  }]);
+	function($scope, Staff) {
+	$scope.staff = Staff.query();
+	}]);
 
 appControllers.controller('ContactCtrl', ['$scope',
-  function($scope) {
-    /** Leaflet Configuration**/
-    angular.extend($scope, {
-        center: {
-            lat: +'43.310995',
-            lng: +'1.935181',
-            zoom: +'09'
-          },
-          markers: {
-            marker: {
-                lat: +'43.110995',
-                lng: +'1.935181',
-                message: '<strong>Festival Arieg\'n ZIC</strong><br/>' +
-                         '09500 Malegoude<br/><br/>' +
-                         'itinéraire : <a target="blank" href="https://maps.google.fr/maps?daddr=malegoude&hl=fr&ll=43.206177,2.109375&spn=0.912897,2.113495&sll=42.943856,1.50092&sspn=1.833603,4.22699&mra=ls&t=m&z=10">google map</a>',
-                focus: true,
-                draggable: false
-              }
-            },
-            defaults: {
-              scrollWheelZoom: false
-            },
-            layers: {
-                baselayers: {
-                      maptile: {
-                        name: 'B/W',
-                        type: 'xyz',
-                        subdomains: '1234',
-                        url: 'http://{s}.maptile.lbs.ovi.com/maptiler/v2/maptile/newest/normal.day.grey/{z}/{x}/{y}/256/png8?token={devID}&app_id={ID}',
-                        layerParams: {
-                          devID: 'xyz',
-                          ID: 'abc',
-                          subdomains: '1234',
-                        }
-                      },
-                    osm: {
-                        name: 'OpenStreetMap   ',
-                        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        type: 'xyz'
-                      }
+	function($scope) {
+	/** Leaflet Configuration**/
+	angular.extend($scope, {
+		center: {
+			lat: +'43.310995',
+			lng: +'1.935181',
+			zoom: +'09'
+		},
+		markers: [
+			{
+				lat: +'43.110995',
+				lng: +'1.935181',
+				message: '<strong>Festival Arieg\'n ZIC</strong><br/>' +
+						 '09500 Malegoude<br/><br/>' +
+						 'itinéraire : <a target="blank" href="https://maps.google.fr/maps?daddr=malegoude&hl=fr&ll=43.206177,2.109375&spn=0.912897,2.113495&sll=42.943856,1.50092&sspn=1.833603,4.22699&mra=ls&t=m&z=10">google map</a>',
+				focus: true,
+				draggable: false
+			},{
+				lat: +'43.103054',
+				lng: +'1.633945',
+				message: '<strong>Pamiers</strong><br/>' +
+						 '30 kms<br/>',
+				focus: false,
+				draggable: false
+			},{
+				lat: +'43.604957', 
+				lng: +'1.447346',
+				message: '<strong>Toulouse</strong><br/>' +
+						 '100 kms<br/>',
+				focus: false,
+				draggable: false
+			},{
+				lat: +'43.213306',
+				lng: +'2.345991',
+				message: '<strong>Carcasonne</strong><br/>' +
+						 '45 kms<br/>',
+				focus: false,
+				draggable: false
+			},{
+				lat: +'42.721688',
+				lng: +'1.837420',
+				message: '<strong>Ax-Les-Thermes</strong>',
+				focus: false,
+				draggable: false
+			}
+		],
+		defaults: {
+			scrollWheelZoom: false
+		},
+		layers: {
+			baselayers: {
+				maptile: {
+					name: 'B/W',
+					type: 'xyz',
+					subdomains: '1234',
+					url: 'http://{s}.maptile.lbs.ovi.com/maptiler/v2/maptile/newest/normal.day.grey/{z}/{x}/{y}/256/png8?token={devID}&app_id={ID}',
+					layerParams: {
+						devID: 'xyz',
+						ID: 'abc',
+						subdomains: '1234',
+					}
+				},
+				osm: {
+					name: 'OpenStreetMap   ',
+					url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+					type: 'xyz'
+				}
 
-                    }
-                  }
-                });
-  }]);
+			}
+		}
+	});
+	}]);
 
 
 
 /*
 appControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+	function($scope, $routeParams, Phone) {
+	$scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+		$scope.mainImageUrl = phone.images[0];
+	});
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
+	$scope.setImage = function(imageUrl) {
+		$scope.mainImageUrl = imageUrl;
+	}
+	}]);
 
 */
